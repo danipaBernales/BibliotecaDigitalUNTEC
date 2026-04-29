899abb7113001c6301343166e7b8704822d20f5e
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2026-04-29 15:53:14 -0400

    Create Usuario.java

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Usuario {
+
+    private int id;
+    private String nombre;
+    private String correo;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id,
+                   String nombre,
+                   String correo,
+                   String password) {
+
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
